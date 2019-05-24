1026b7d986fdb6112d13a0c4a04960dee3952340
alarai <wedge@atlanteans.net>
2019-05-24 12:40:16 +0200

    Implement categories management

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Categories;
+use App\Form\CategoriesType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController {
@@ -12,14 +14,43 @@ class CategoryController extends AbstractController {
      */
     public function index() {
 
-        return $this->render("category/category.html.twig");
+        $categories = $this->getDoctrine()->getRepository(Categories::class)->findBy([], ['nom' => 'ASC']);
+
+        return $this->render("category/category.html.twig", ['categories' => $categories]);
+    }
+
+    private function getCategory($id) {
+        if($id !== null) {
+            $category = $this->getDoctrine()->getRepository(Categories::class)->find($id);
+            if($category === null) {
+                return $this->redirectToRoute('category');
+            }
+        } else {
+            $category = new Categories();
+        }
+
+        return $category;
     }
 
     /**
      * @Route("/category/edit/{id<\d+>}", name="category_edit", defaults={"id"=null})
      */
-    public function edit($id) {
-        return new Response('<html><body>category edit ' . $id . '</body></html>');
+    public function edit($id, Request $request) {
+        $category = $this->getCategory($id);
+
+        $form = $this->createForm(CategoriesType::class, $category);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($category);
+            $em->flush();
+            $this->addFlash('success', 'Catégorie ' . ($id===null?"ajouté":"mis à jour"));
+            return $this->redirectToRoute('category');
+        }
+
+        return $this->render('category/edit.html.twig', ['form' => $form->createView()]);
     }
 
 }
