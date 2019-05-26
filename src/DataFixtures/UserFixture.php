d078310da325fb28463a584482106f4913db4560
alarai <wedge@atlanteans.net>
2019-05-26 16:26:52 +0200

    Implemented security

---
+++ src/DataFixtures/UserFixture.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixture extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $user = new User();
+        $user->setUsername('wedge');
+        $user->setPassword($this->passwordEncoder->encodePassword($user, 'Y7tnm5KMhGpk'));
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
